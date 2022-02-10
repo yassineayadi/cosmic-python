6c084a37b661ab3d07bc282c5acfa2a1413d8600
yassineayadi <ayadi.yas@gmail.com>
2022-02-10 19:56:06 +0100

    [Alembic] move migration folder to top-level and integrate support for db schema creation in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,21 +6,26 @@ RUN apk --update --upgrade add gcc musl-dev jpeg-dev zlib-dev libffi-dev cairo-d
 # setup application user
 RUN adduser -D allocation
 USER allocation
-
-WORKDIR /home/allocation
+ENV APP_PATH /home/allocation
+WORKDIR $APP_PATH
 
 # setup virtual environment
 RUN python -m venv venv
-ENV VIRTUAL_ENV venv
+ENV VIRTUAL_ENV $APP_PATH/venv
 ENV PATH $VIRTUAL_ENV/bin:$PATH
 COPY --chown=allocation requirements.txt requirements.txt
-RUN pip install --upgrade pip && pip install -r requirements.txt
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
 
 # copy application files
 COPY --chown=allocation src src
 COPY --chown=allocation tests tests
 COPY --chown=allocation setup.py pyproject.toml tox.ini ./
 RUN pip install -e .
+
+# setup db
+ENV DB_PATH $APP_PATH/prod.db
+RUN cd src && alembic upgrade head
 
 ENV FLASK_APP src/allocation/entrypoints/app.py
 ENV FLASK_ENV production
--- src/allocation/config.py
+++ src/allocation/config.py
@@ -6,39 +6,41 @@ CURRENT_DIRECTORY = Path(__file__).parent
 
 
 class Config:
-    DB_PATH: Path
+    DB_PATH: str
     DB_URI: str
     SQLA_CONNECTION_STRING: str
     DB_TYPE: str
-    REDIS_CONFIG = {"host": "redis", "port": 6379, "db": 0}
+    REDIS_HOST = os.getenv("REDIS_HOST") or "localhost"
+    REDIS_PORT = os.getenv("REDIS_PORT") or 6379
+    REDIS_DB = os.getenv("REDIS_DB") or 0
     SWAGGER = {"title": "Allocation Service"}
 
 
 class DevelopmentConfig(Config):
-    DB_PATH = CURRENT_DIRECTORY / Path("dev.db")
+    DB_PATH = os.getenv("DB_PATH") or "dev.db"
     DB_CONNECTION_SETTINGS = "?mode=rw&check_same_thread=False"
-    DB_URI = DB_PATH.as_uri() + DB_CONNECTION_SETTINGS
-    REDIS_CONFIG = {"host": "localhost", "port": 6379, "db": 0}
+    DB_URI = f"file:{DB_PATH}" + DB_CONNECTION_SETTINGS
     SQLA_CONNECTION_STRING = f"sqlite:///{DB_PATH}" + DB_CONNECTION_SETTINGS
-    DB_TYPE = "SQlite3"
+    DB_TYPE = os.getenv("DB_TYPE") or "SQlite3"
 
 
 class TestingConfig(Config):
     DB_PATH = CURRENT_DIRECTORY
     DB_CONNECTION_SETTINGS = "?mode=rw&check_same_thread=False&cache=shared"
     DB_URI = ""
-    REDIS_CONFIG = {"host": "localhost", "port": 6379, "db": 0}
     SQLA_CONNECTION_STRING = "sqlite:///:memory:" + DB_CONNECTION_SETTINGS
-    DB_TYPE = "MEMORY"
+    DB_TYPE = os.getenv("DB_TYPE") or "MEMORY"
 
 
 class ProductionConfig(Config):
-    DB_PATH = CURRENT_DIRECTORY / Path("dev.db")
+    DB_PATH = os.getenv("DB_PATH") or "prod.db"
     DB_CONNECTION_SETTINGS = "?mode=rw&check_same_thread=False"
-    REDIS_CONFIG = {"host": "redis", "port": 6379, "db": 0}
-    DB_URI = DB_PATH.as_uri() + DB_CONNECTION_SETTINGS
+    DB_URI = f"file:{DB_PATH}" + DB_CONNECTION_SETTINGS
     SQLA_CONNECTION_STRING = f"sqlite:///{DB_PATH}" + DB_CONNECTION_SETTINGS
     DB_TYPE = "SQlite3"
+    REDIS_HOST = os.getenv("REDIS_HOST") or "redis"
+    REDIS_PORT = os.getenv("REDIS_PORT") or 6379
+    REDIS_DB = os.getenv("REDIS_DB") or 0
 
 
 config = {
@@ -57,4 +59,4 @@ def get_config(config_name=None) -> Config:
 
 def get_redis_config() -> Dict:
     c = get_config()
-    return c.REDIS_CONFIG
+    return {"host": c.REDIS_HOST, "port": c.REDIS_PORT, "db": c.REDIS_DB}
