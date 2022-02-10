45ef9fdc7e99d12d2a85515ddab6a83f596a8da2
yassineayadi <ayadi.yas@gmail.com>
2022-02-10 18:49:00 +0100

    [Build] Remove tox build directory from git

--- build/lib/allocation/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from typing import Union
-
-from .commands import Command
-from .events import Event
-
-Message = Union[Event, Command]
--- build/lib/allocation/interfaces/database/utils.py
+++
@@ -1,7 +0,0 @@
-from allocation.config import get_config
-
-
-def create_db_if_no_exists():
-    config = get_config()
-    if not config.DB_PATH.exists():
-        config.DB_PATH.touch()
